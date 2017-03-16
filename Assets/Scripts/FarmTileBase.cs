3cfc55ddaed74d54c37a0f29c11bcdc826aa9742
Gerdie <mariakmoy@gmail.com>
2017-03-15 20:24:32 -0700

    Adding farmtile script

---
+++ Assets/Scripts/FarmTileBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmTileBase : MonoBehaviour {
+	[SerializeField] private Sprite farm_tile;
+	[SerializeField] private Sprite soil_tile;
+
+	public void OnMouseDown() {
+		GetComponent<SpriteRenderer> ().sprite = soil_tile;
+	}
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().sprite = farm_tile;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
